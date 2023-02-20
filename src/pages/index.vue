4c39b417b18175411e7e5c5b99d5af1ef17d20a8
HyperVendy <me@trentvendy.com>
2023-02-21 10:04:42 +1100

    update copy to match resume

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,24 +2,33 @@
   <div class="m-auto">
     <TheProfilePhoto width="250" />
   </div>
-  <div prose m-auto mb-8>
-    <h1 mb-0>
+  <div class="prose m-auto mb-8">
+    <h1>
       Trent Vendy
     </h1>
     <h3>
-      Platform Engineer - Full Stack Developer - Mental Health Advocate
+      Full Stack Cloud Engineer
     </h3>
-  </div>
-
-  <div prose m-auto p-2>
-    Hey, I'm Trent Vendy, a Platform Engineer, Full Stack Developer, and Mental Health Advocate.
-  </div>
-
-  <div class="prose m-auto p-2">
-    I like building stuff, whether it's software like games, apps, or APIs; to physical things like computers, cat towers, 3D printed gadgets and gizmos, and most recently even my own outdoor decking!
+    <h5>
+      SaaS | FinTech | Front End | Back End | Infrastructure | CI/ CD
+    </h5>
   </div>
 
   <div class="prose m-auto p-2">
-    Something I like even more is people. I've recently discovered just how much I love helping, teaching, and trying to find ways to better myself and others. I love the challenge of figuring out how someone learns, and how I could pass on my knowledge in such a way that suits them.
+    Hey, I'm Trent Vendy, a Full Stack Cloud Engineer with a passion for teaching.
+  </div>
+
+  <div class="prose m-auto p-2">
+    I like building stuff. Whether that's on a computer, like video games, apps, or APIs, or physical things like computers, cat towers, 3D printed gadgets and gizmos, and most recently even my own outdoor decking!
+  </div>
+
+  <div class="prose m-auto p-2">
+    I've recently discovered just how much I love helping people, teaching and trying to find ways to better myself and others. I love the challenge of figuring out how someone learns, and how I could pass on my knowledge in such a way that suits them.
+  </div>
+
+  <div class="prose m-auto mb-8">
+    <h3>
+      I am currently looking for a new role, so if you're intereseted, please reach out via LinkedIn or email :D
+    </h3>
   </div>
 </template>
